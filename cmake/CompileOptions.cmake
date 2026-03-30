1a52a67eaf6e1678712796aae5031696e509bd01
Richard Dong <66289396+rdong8@users.noreply.github.com>
2026-03-29 23:58:13 -0400

    feat: move cmake vars to conan profile

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -4,6 +4,9 @@ function(set_project_compile_options project_name)
             "$<$<CXX_COMPILER_ID:GCC>:-ggdb>"
             "-fno-omit-frame-pointer"
             "-fno-sanitize-merge"
+        # Turn on this flag only if you need to run LLDB
+        # It interferes with stack traces
+        # TODO: remove disclaimer when this is resolved https://github.com/jeremy-rifkin/cpptrace/issues/307
             "-gmodules"
     )
 
@@ -24,6 +27,8 @@ function(set_project_compile_options project_name)
             "-D_FORTIFY_SOURCE=1"
             "-O3"
             "-march=native"
+        #     Enable if profiling
+        #     "-pg"
     )
 
     target_compile_options("${project_name}"
